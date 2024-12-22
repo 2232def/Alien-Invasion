55fa3941e6ac0dc4e7deacd171f8edd2b8335f30
Dev Kumar Singh <gohi772004@gmail.com>
2024-12-22 13:37:21 +0530

    added asyncio

--- settings.py
+++ settings.py
@@ -10,19 +10,19 @@ class settings:
         self.bg_color = (0, 255, 255)
 
         #ship settings
-        self.ship_speed = 1.5
+        self.ship_speed = 2.5
         self.ship_limit = 3
 
         #bullet settings
-        self.bullet_speed = 2.5
+        self.bullet_speed = 3.5
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
-        self.bullets_allowed = 10
+        self.bullets_allowed = 8
 
         #alien settings
         self.alien_speed = 1.0
-        self.fleet_drop_speed = 10
+        self.fleet_drop_speed = 8
         # Fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
 
